83c832c7de2742ff7d9b2763b8fae04bbc486bf5
Kelvin Brito <britok30@gmail.com>
2021-04-14 09:06:11 -0400

    build out chat component structure

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Chat.css';
 import ChatHeader from './ChatHeader';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
 const Chat = () => {
     return (
@@ -10,7 +14,19 @@ const Chat = () => {
             <div className="chat__messages"></div>
 
             <div className="chat__input">
-                
+                <AddCircleIcon fontSize="large" />
+                <form>
+                    <input placeHolder={`Message #TESTCHANNEL`} />
+                    <button className="chat__inputButton" type="submit">
+                        Send Message
+                    </button>
+                </form>
+
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontSize="large" />
+                    <GifIcon fontSize="large" />
+                    <EmojiEmotionsIcon fontSize="large" />
+                </div>
             </div>
         </div>
     );
