7bc5793da7e46a98720045d8228e1f40d36733bb
Kelvin Brito <britok30@gmail.com>
2021-04-15 05:53:07 -0400

    set up user reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counterSlice';
+import userReducer from '../features/userSlice';
 
 export const store = configureStore({
     reducer: {
